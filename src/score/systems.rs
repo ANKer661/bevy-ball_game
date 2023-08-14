3b48c4b63347d026d388bb25c9df25e5ae55e290
bobo_win <1229316440@qq.com>
2023-08-14 12:12:02 +0800

    project refactor

---
+++ src/score/systems.rs
@@ -0,0 +1,24 @@
+use super::resources::*;
+use crate::events::*;
+use bevy::prelude::*;
+
+pub fn update_score(score: Res<Score>) {
+    if score.is_changed() {
+        println!("Score: {}", score.value.to_string())
+    }
+}
+
+pub fn update_high_score(
+    mut game_over_envent_reader: EventReader<GameOver>,
+    mut high_scores: ResMut<HighScore>,
+) {
+    for event in game_over_envent_reader.iter() {
+        high_scores.scores.push(("Player".to_string(), event.score))
+    }
+}
+
+pub fn high_score_updated(high_scores: Res<HighScore>) {
+    if high_scores.is_changed() {
+        println!("High socre: {:?}", high_scores);
+    }
+}
